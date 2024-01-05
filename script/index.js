a17b2e467988328799f7d68213a6f907d776cdc1
Maki <makiangejmj16@gmail.com>
2024-01-05 19:49:43 +0100

    first commit

---
+++ script/index.js
@@ -0,0 +1,28 @@
+// Modal Image Gallery
+function onClick(element) {
+    document.getElementById("img01").src = element.src;
+    document.getElementById("modal01").style.display = "block";
+    var captionText = document.getElementById("caption");
+    captionText.innerHTML = element.alt;
+}
+
+// Change style of navbar on scroll
+window.onscroll = function () { myFunction() };
+function myFunction() {
+    var navbar = document.getElementById("myNavbar");
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        navbar.className = "bar_menu_js" + "bar_menu_js_card" + "bar_menu_js_animatetop" + " bar_menu_js_white";
+    } else {
+        navbar.className = navbar.className.replace(" bar_menu_js_card bar_menu_js_animatetop bar_menu_js_white", "");
+    }
+}
+
+// Used to toggle the menu on small screens when clicking on the menu button
+function toggleFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
