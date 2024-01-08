54748f74587c7c7af17d10c9729c20d156facefc
Maki <makiangejmj16@gmail.com>
2024-01-08 19:42:11 +0100

    Site renouvellement designe

--- script.js
+++ script.js
@@ -2,6 +2,12 @@
 let nextDom = document.getElementById('next');
 let prevDom = document.getElementById('prev');
 
+// variable text
+let homeDom = document.getElementById('home');
+let realisationDom = document.getElementById('realisation');
+let qualificationDom = document.getElementById('qualification');
+let contactDom = document.getElementById('contact');
+
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
@@ -9,37 +15,75 @@ let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+// SliderDom.appendChild(SliderItemsDom[0]);
 let timeRunning = 3000;
 // let timeAutoNext = 7000;
 
-nextDom.onclick = function(){
-    showSlider('next');    
+homeDom.onclick = function () {
+    showSlider('home');
 }
 
-prevDom.onclick = function(){
-    showSlider('prev');    
+realisationDom.onclick = function () {
+    showSlider('realisation');
+}
+
+qualificationDom.onclick = function () {
+    showSlider('qualification');
+}
+contactDom.onclick = function () {
+    showSlider('contact');
 }
 let runTimeOut;
 // let runNextAuto = setTimeout(() => {
 //     next.click();
 // }, timeAutoNext)
-function showSlider(type){
-    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-    
-    if(type === 'next'){
-        SliderDom.appendChild(SliderItemsDom[0]);
-        thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
-        carouselDom.classList.add('next');
-    }else{
-        SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
-        thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
-        carouselDom.classList.add('prev');
+
+    switch (type) {
+
+        case 'home':
+            // SliderDom.remove();
+            SliderDom.appendChild(SliderItemsDom[0]);
+            // SliderDom[0];
+            thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
+            carouselDom.classList.add('home');
+            break;
+
+        case 'realisation': 
+            // SliderDom.remove();
+            SliderDom.prepend(SliderItemsDom[1]);
+            thumbnailBorderDom.prepend(thumbnailItemsDom[1]);
+            carouselDom.classList.add('realisation');
+            // SliderDom.prepend(SliderItemsDom[1]);
+            // thumbnailBorderDom.prepend(thumbnailItemsDom[1]);
+            // carouselDom.classList.add('realisation');
+            break;
+        case 'qualification':
+            SliderDom.prepend(SliderItemsDom[2]);
+            thumbnailBorderDom.prepend(thumbnailItemsDom[2]);
+            carouselDom.classList.add('qualification');
+            break;
+        case 'contact':
+            SliderDom.prepend(SliderItemsDom[3]);
+            thumbnailBorderDom.prepend(thumbnailItemsDom[3]);
+            carouselDom.classList.add('realisation');
+            break;
+
     }
+
+
+
+
+
     clearTimeout(runTimeOut);
     runTimeOut = setTimeout(() => {
-        carouselDom.classList.remove('next');
-        carouselDom.classList.remove('prev');
+        carouselDom.classList.remove('home');
+        carouselDom.classList.remove('realisation');
+        carouselDom.classList.remove('qualification');
+        carouselDom.classList.remove('contact');
+
     }, timeRunning);
 
     // clearTimeout(runNextAuto);
